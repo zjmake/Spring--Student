0c426853b51eb3ff2715bfd6ee4ca5851ccbf8ed
ZJ0703 <you@example.com>
2019-08-08 14:08:06 +0800

    更新

--- spring_boot/src/main/java/com/zj/spring_boot/test/ControllerTest.java
+++ spring_boot/src/main/java/com/zj/spring_boot/test/ControllerTest.java
@@ -1,13 +1,30 @@
 package com.zj.spring_boot.test;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
 public class ControllerTest {
 	
-	@RequestMapping("/select")
+	
+	@Autowired
+    private DiscoveryClient client;
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String select() {
+		
+		List<ServiceInstance> instances = client.getInstances("hello-service");
+		for (int i = 0; i < instances.size(); i++) {
+            System.err.println("Host"+instances.get(i).getHost()+"ServiceId"+instances.get(i).getServiceId());
+        }
 		return "Hello World!";
 	}
 
